e0470a587c274b2ab115fedcd95c8690d37b2d3d
Clayton Crispim <43951382+claytoncrispim@users.noreply.github.com>
2024-11-05 10:38:08 +0000

    update

---
+++ bill_calculator_6.1/components/Total.js
@@ -0,0 +1,32 @@
+/**
+ * Total Object:
+ * ID
+ * Name
+ * Amount AIB
+ * Amount BOI
+ * Sub-total
+ * Total (less Amount BOI)
+ */
+
+class Total {
+    constructor(
+      id, 
+      name, 
+      amtAib, 
+      amtBoi,
+      subTotal, 
+      total
+    ) {
+      this.id = id;
+      this.name = name;
+      this.amtAib = amtAib;
+      this.amtBoi = amtBoi;
+      this.subTotal = subTotal;
+      this.total = total;
+    }
+    // updateAmount(amount) {
+    //   this.amount = amount;
+    // }
+  }
+  
+  export default Total;
